a2b630e37fdbb9b04f3c9b7514d8034b20acb047
Hasan Abdullah <hsn.abdullah282@gmail.com>
2024-01-19 15:42:00 +0700

    fix migrations

--- app/Database/Migrations/20240119033039_Principal.php
+++ app/Database/Migrations/20240119033039_Principal.php
@@ -9,52 +9,60 @@ class Principal extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id' => [
-                'type' => 'VARCHAR',
-                'constraint' => '16',
+            'id'          => [
+                'type'       => 'VARCHAR',
+                'constraint' => 16
             ],
-            'hash' => [
-                'type' => 'VARCHAR',
-                'constraint' => '30',
-                'default' => null,
+            'hash'        => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
+                'null'       => true,
+                'default'    => null
             ],
-            'principal' => [
-                'type' => 'VARCHAR',
-                'constraint' => '128',
-                'default' => null
+            'principal'   => [
+                'type'       => 'VARCHAR',
+                'constraint' => 128,
+                'null'       => true,
+                'default'    => null
             ],
-            'address' => [
-                'type' => 'VARCHAR',
-                'constraint' => '250',
-                'default' => null
+            'address'     => [
+                'type'       => 'VARCHAR',
+                'constraint' => 250,
+                'null'       => true,
+                'default'    => null
             ],
-            'phone' => [
-                'type' => 'VARCHAR',
-                'constraint' => '16',
-                'default' => null
-            ],
-            'email' => [
-                'type' => 'VARCHAR',
-                'constraint' => '32',
-                'default' => null
-            ],
-            'id_agent' => [
-                'type' => 'VARCHAR',
+            'phone'       => [
+                'type'       => 'VARCHAR',
                 'constraint' => 16,
-                'default' => null
+                'null'       => true,
+                'default'    => null
             ],
-            'active' => [
-                'type' => 'TINYINT',
-                'constraint' => '1'
+            'email'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => 64,
+                'null'       => true,
+                'default'    => null
+            ],
+            'id_agent'    => [
+                'type'       => 'VARCHAR',
+                'constraint' => 16,
+                'null'       => true,
+                'default'    => null
+            ],
+            'active'      => [
+                'type'       => 'TINYINT',
+                'unsigned'   => true,
+                'constraint' => 1,
+                'default'    => 0
             ]
         ]);
+
         $this->forge->addPrimaryKey('id', 'PRIMARY');
-        $this->forge->addKey('active', false, false, 'ACTIVE');
-        $this->forge->addKey('id_agent', false, false, 'AGENT');
-
-        $this->forge->addForeignKey('id_agent', 'd_agent', 'id', 'CASCADE', 'USERAGENT');
-
         $this->forge->addUniqueKey('hash', 'PARAM');
+        $this->forge->addKey('id_agent', false, false, 'AGENT');
+        $this->forge->addKey('active', false, false, 'ACTIVE');
+
+        $this->forge->addForeignKey('id_agent', 'd_agent', 'id', 'CASCADE', 'SET NULL', 'PRINCIPALAGENT');
 
         $this->forge->createTable('d_principal', true, [
             'ENGINE' => 'InnoDB'
--- app/Database/Migrations/20240119033053_PrincipalSigner.php
+++ app/Database/Migrations/20240119033053_PrincipalSigner.php
@@ -9,43 +9,48 @@ class PrincipalSigner extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id' => [
-                'type' => 'VARCHAR',
-                'constraint' => '16'
+            'id'           => [
+                'type'       => 'VARCHAR',
+                'constraint' => 16
             ],
-            'hash' => [
-                'type' => 'VARCHAR',
-                'constraint' => '30',
-                'default' => null
+            'hash'         => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
+                'null'       => true,
+                'default'    => null
             ],
             'id_principal' => [
-                'type' => 'VARCHAR',
-                'constraint' => '16',
-                'default' => null
+                'type'       => 'VARCHAR',
+                'constraint' => 16,
+                'null'       => true,
+                'default'    => null
             ],
-            'nama' => [
-                'type' => 'VARCHAR',
-                'constraint' => '64',
-                'default' => null
+            'nama'         => [
+                'type'       => 'VARCHAR',
+                'constraint' => 64,
+                'null'       => true,
+                'default'    => null
             ],
-            'position' => [
-                'type' => 'VARCHAR',
-                'constraint' => '32',
-                'default' => null
+            'position'     => [
+                'type'       => 'VARCHAR',
+                'constraint' => 64,
+                'null'       => true,
+                'default'    => null
             ],
-            'active' => [
-                'type' => 'TINYINT',
-                'constraint' => '1',
-                'default' => null
+            'active'       => [
+                'type'       => 'TINYINT',
+                'unsigned'   => true,
+                'constraint' => 1,
+                'default'    => 0
             ],
         ]);
+
         $this->forge->addPrimaryKey('id', 'PRIMARY');
-        $this->forge->addKey('active', false, false, 'ACTIVE');
-        $this->forge->addKey('id_principal', false, false, 'PRINCIPAL');
-
-        $this->forge->addForeignKey('id_principal', 'd_principal', 'id', 'CASCADE', 'IDPRINCIPAL');
-
         $this->forge->addUniqueKey('hash', 'PARAM');
+        $this->forge->addKey('id_principal', false, false, 'PRINCIPAL');
+        $this->forge->addKey('active', false, false, 'ACTIVE');
+
+        $this->forge->addForeignKey('id_principal', 'd_principal', 'id', 'CASCADE', 'SET NULL', 'SIGNERPRINCIPAL');
 
         $this->forge->createTable('d_principal_signer', true, [
             'ENGINE' => 'InnoDB'
